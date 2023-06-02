38d2cfd31fabde72deb5654c6db4e1c49f3d8e7b
= <=>
2023-06-02 23:26:23 +0700

    first commit

---
+++ src/main/java/com/frinz/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,71 @@
+package com.frinz.tutorialmod.block;
+
+import com.frinz.tutorialmod.TutorialMod;
+import com.frinz.tutorialmod.item.ModCreativeModeTab;
+import com.frinz.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block",
+            () -> new Block(
+                    BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(1)
+                            .requiresCorrectToolForDrops()
+            ),
+            ModCreativeModeTab.TUTORIAL_TAB
+    );
+
+    public static final RegistryObject<Block> ZIRCON_ORE = registerBlock("zircon_ore",
+            () -> new DropExperienceBlock(
+                    BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(1)
+                            .requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)
+            ),
+            ModCreativeModeTab.TUTORIAL_TAB
+    );
+    public static final RegistryObject<Block> DEEPSLATE_ZIRCON_ORE = registerBlock("deepslate_zircon_ore",
+            () -> new DropExperienceBlock(
+                    BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(2)
+                            .requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)
+            ),
+            ModCreativeModeTab.TUTORIAL_TAB
+    );
+
+    // helper function
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    // helper function
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, ()->new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
